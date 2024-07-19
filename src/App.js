65eb99ccd99ae0f5695d3ca38f74d27013e22789
Kidwit <toj817@gmail.com>
2024-07-18 21:31:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        TUP TUP TUP TUP TUP TUP TUP TUP TUP TUP TUP
         </p>
         <a
           className="App-link"
