2479d708bd53ae286e6f1e197edd95510fc882b1
Kidwit <toj817@gmail.com>
2024-07-22 20:55:43 -0700

    Added placeholders

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -1,8 +1,30 @@
-function Create () {
+import React from "react"
+
+function Create() {
     return (
-        <p>Placeholder</p>
+        <div>
+            <div>
+                <h2>Create new document</h2>
+            </div>
+            <div>
+                <h3>Select Template Here:</h3>
+                <span>Template option 1 </span>
+                <span>Template Option 2</span>
+                <span>Template Option 3</span>
+            </div>
+            <div>
+                <h3>Settings Selection</h3>
+                <ul>
+                    <li>Font</li>
+                    <li>Text Color</li>
+                    <li>Access(question mark)?</li> 
+                    
+                </ul>
+            </div>
+        </div>
     )
 }
+
 
 
 export default Create
