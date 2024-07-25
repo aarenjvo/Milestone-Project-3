2bffd41a53c4d4a32683b0c228407b4eedd519fc
Kidwit <toj817@gmail.com>
2024-07-25 00:20:38 -0700

    Added to create component and linked homepage

--- frontend/src/views/home.jsx
+++ frontend/src/views/home.jsx
@@ -1,11 +1,21 @@
-function home() {
+function Home() {
 
   return(
-    <header>
-      <h1>Chat GBT</h1>
-    </header>
+  <div>
+    <nav>
+      <a><h3>Make your creation</h3></a>
+      <h3>View Posts</h3>
+      <div className="title"><h2>TupGPT</h2></div>
+      <h3>Sign Up</h3>
+      <h3>Log In</h3>
+    </nav>
+    <div className="welcome">
+      <h1>Welcome!</h1>
+      <h3>This is a project created by Freddy Diaz, Aaren Vo,Anthony Deguire, and Tj Jones</h3>
+    </div>
+    </div>
   )
 
 }
 
-export default home
+export default Home
