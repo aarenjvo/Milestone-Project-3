291c9c641d241283b5aa9af123c58d06bb80ca30
Kidwit <toj817@gmail.com>
2024-07-25 18:48:45 -0700

    Create,js connected to homejsx

--- frontend/src/views/home.jsx
+++ frontend/src/views/home.jsx
@@ -1,9 +1,26 @@
+import React, { useState } from "react"
+import Create from "../components/Create"
+
 function Home() {
+  const [showCreatePopup, setShowCreatePopup] = useState(false);
+
+  const handleCreateClick = () => {
+    setShowCreatePopup(true);
+  };
+
+  const handleCloseCreatePopup = () => {
+    setShowCreatePopup(false);
+  };
+
+  const handleCreateDocument = (documentData) => {
+    console.log("Document created:", documentData);
+    <Create/>
+  };
 
   return(
   <div>
     <nav>
-      <a><h3>Make your creation</h3></a>
+    <a href="#" onClick={handleCreateClick}><h3>Make your creation</h3></a>
       <h3>View Posts</h3>
       <div className="title"><h2>TupGPT</h2></div>
       <h3>Sign Up</h3>
@@ -13,7 +30,11 @@ function Home() {
       <h1>Welcome!</h1>
       <h3>This is a project created by Freddy Diaz, Aaren Vo,Anthony Deguire, and Tj Jones</h3>
     </div>
+    {showCreatePopup && (
+        <Create onClose={handleCloseCreatePopup} onCreate={handleCreateDocument} />
+      )}
     </div>
+    
   )
 
 }
