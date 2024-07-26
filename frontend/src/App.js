8e628c4c37ac492bd002eec711e48184ff8e6e71
Kidwit <toj817@gmail.com>
2024-07-26 16:30:12 -0700

    Fixed minor merging issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 import Home from './views/home';
 import Creation from './views/creation';
 
@@ -9,16 +9,11 @@ function App() {
     <div className='App'>
       <Router>
         <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/Creation" element={<Creation/>}/>
-      </Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Creation" element={<Creation />} />
+        </Routes>
       </Router>
-      </div>
-
-
-function App() {
-  return (
-      <Creation/>
+    </div>
   );
 }
 
