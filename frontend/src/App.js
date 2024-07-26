65a2867b03c939bf829c3a8ab93bd829106fe98f
“Anthony <anthony.deguire619@gmail.com>
2024-07-25 20:53:47 -0700

    blog page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './views/home';
+import Creation from './views/creation';
 
 function App() {
   return (
-      <Home/>
+      <Creation/>
   );
 }
 
--- frontend/src/views/creation.jsx
+++ frontend/src/views/creation.jsx
@@ -1,8 +1,42 @@
-function creation() {
+function Creation() {
   
   return(
-    <h1>PlaceHolder</h1>
+    <div>
+      <header class="header">
+        <h1>Blog Postings</h1>
+      </header>
+      <main class="main">
+        <form id="blogPostings">
+          <input type="text" name="name" placeholder="Thoughts..."></input>
+          <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </main>
+      <div>
+        <ul>
+          <li>
+            <span>Anthony</span>
+            <button>Edit</button>
+            <button>Remove</button>
+          </li>
+          <li>
+          <span>Aaren</span>
+            <button>Edit</button>
+            <button>Remove</button>
+          </li>
+          <li>
+          <span>TJ</span>
+            <button>Edit</button>
+            <button>Remove</button>
+            <li>
+            <span>Freddy</span>
+            <button>Edit</button>
+            <button>Remove</button>
+            </li>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
-export default creation
+export default Creation
